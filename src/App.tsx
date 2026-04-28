ba7169db48d9bb6d0db2348baaab560b83322718
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-04-28 16:29:38 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import FoodDeliveryForm from "./component/FoodDeliveryForm";
+
+function App() {
+    
+    return (
+        <div className="bg-red-400">
+            <FoodDeliveryForm />
+        </div>
+    );
+}
+
+export default App;
