c7e75a2ac11679c1f7870eafcf60cb5136f47590
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-04-30 11:19:07 +0330

    add register from Props

--- src/component/RHF.tsx
+++ src/component/RHF.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import Input from "./Input";
+import Input from "./controls/textField";
 import {
     foodDeliverySchema,
     type FoodDeliveryFormType,
@@ -35,77 +35,51 @@ export default function Rhf() {
                 onSubmit={handleSubmit(onSubmit, onError)}
                 className="space-y-8"
             >
-                <div className="flex mb-2">
+                <div className="flex gap-1 mb-2">
                     <div className="flex flex-col">
-                        <div className="relative">
-                            <input
-                                type="number"
-                                id="orderNo"
-                                className=" text-sm text-white w-full px-4 pt-5 pb-2 border border-gray-500 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 peer transition-all"
-                                placeholder=""
-                                {...register("orderNo", {
-                                    valueAsNumber: true,
-                                    required: "order number is required",
-                                })}
-                            />
-                            <label
-                                htmlFor="orderNo"
-                                className=" absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 transition-all duration-200 pointer-events-none peer-focus:top-2 peer-focus:text-xs peer-focus:text-blue-500 peer-focus:-translate-y-0 peer-[:not(:placeholder-shown)]:top-2 peer-[:not(:placeholder-shown)]:text-xs peer-[:not(:placeholder-shown)]:-translate-y-0 px-1"
-                            >
-                                Order Number
-                            </label>
-                            {errors.orderNo?.message && (
-                                <p className="error">
-                                    {errors.orderNo?.message}
-                                </p>
-                            )}
-                        </div>
+                        {/* use Reusable Input component */}
+                        <Input
+                            error={errors.orderNo?.message}
+                            {...register("orderNo", {
+                                valueAsNumber: true,
+                                required: "order number is required",
+                            })}
+                            id="orderNo"
+                            label="order Number"
+                        />
                     </div>
                     <div className="flex flex-col">
-                        <div className="relative">
-                            <input
-                                type="email"
-                                id="email"
-                                className=" text-sm text-white w-full px-4 pt-5 pb-2 border border-gray-500 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 peer transition-all"
-                                placeholder=""
-                                {...register("email", {
-                                    required: "email is required",
-                                    validate: {
-                                        notFake: (value) => {
-                                            console.log("Validating:", value);
-                                            if (value === "email@gmail.com") {
-                                                return "particular email is blocked";
-                                            }
-                                            return true;
-                                        },
-                                    }
-                                })}
-                            />
-                            <label
-                                htmlFor="email"
-                                className=" absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 transition-all duration-200 pointer-events-none peer-focus:top-2 peer-focus:text-xs peer-focus:text-blue-500 peer-focus:-translate-y-0 peer-[:not(:placeholder-shown)]:top-2 peer-[:not(:placeholder-shown)]:text-xs peer-[:not(:placeholder-shown)]:-translate-y-0 px-1"
-                            >
-                                email
-                            </label>
-                            {errors.email?.message && (
-                                <p className="error">{errors.email?.message}</p>
-                            )}
-                        </div>
+                        <Input
+                            error={errors.email?.message}
+                            id="email"
+                            label="Email"
+                            {...register("email", {
+                                required: "email is required",
+                                validate: {
+                                    notFake: (value) => {
+                                        console.log("Validating:", value);
+                                        if (value === "email@gmail.com") {
+                                            return "particular email is blocked";
+                                        }
+                                        return true;
+                                    },
+                                },
+                            })}
+                        />
                     </div>
                 </div>
 
-                {/* use Reusable Input component */}
                 <Input
                     id="customerName"
                     label="Customer Name"
-                    register={register}
+                    {...register("customerName")}
                     error={errors.customerName?.message}
                 />
 
                 <Input
                     id="mobile"
                     label="Mobile Number"
-                    register={register}
+                    {...register("mobile")}
                     error={errors.mobile?.message}
                 />
                 <button
